245a5e0d003486649d1f3c5d9dfd4111df783f54
Abaddon <113919538+z0zken@users.noreply.github.com>
2024-03-09 14:08:11 +0700

    ..

--- Assets/Script/gameManagement/GameMannager.cs
+++ Assets/Script/gameManagement/GameMannager.cs
@@ -22,15 +22,15 @@ public class GameMannager : MonoBehaviour
     }
     public void Awake()
     {
-        //if (instance != null && instance != this)
-        if (instance != this)
-            {
+        if (instance != null && instance != this)
+        //if (instance != this)
+        {
             Destroy(this.gameObject);
         }
         else
         {
             instance = this;
-            GameObject.DontDestroyOnLoad(this.gameObject);
+            //GameObject.DontDestroyOnLoad(this.gameObject);
         }
 
         tileMannager = GetComponent<TileMannager>();
